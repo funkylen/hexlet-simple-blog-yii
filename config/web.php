3d7701a45765c0a91774ee38df3ba4513864ad11
funkylen <funkylen@gmail.com>
2022-11-20 13:39:55 +0300

    beauty routes

--- config/web.php
+++ config/web.php
@@ -42,14 +42,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '/' => 'site/index',
+                'about' => 'site/about',
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
